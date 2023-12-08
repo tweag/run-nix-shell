2d009b89d9b6655717005d71032dd09b5145c952
Chuck Grindel <chuck.grindel@gmail.com>
2023-12-08 11:16:18 -0700

    Refactor shell.nix

--- tests/integration_tests/shell.nix
+++ tests/integration_tests/shell.nix
@@ -2,22 +2,25 @@
   customVarBool ? false,
   customVarStr ? "default",
   lock ? builtins.fromJSON (builtins.readFile ./../../flake.lock),
-}:
-(import
-  (
+  flakeCompat ?  (
     fetchTarball {
       url = lock.nodes.flake-compat.locked.url or "https://github.com/edolstra/flake-compat/archive/${lock.nodes.flake-compat.locked.rev}.tar.gz";
       sha256 = lock.nodes.flake-compat.locked.narHash;
     }
-  )
-  { src = ./../../.; }
-).shellNix // (
-  let pkgs = import (
+  ),
+  nixpkgs ?  (
     fetchTarball {
       url = "https://github.com/NixOS/nixpkgs/archive/${lock.nodes.nixpkgs.locked.rev}.tar.gz";
       sha256 = lock.nodes.nixpkgs.locked.narHash;
     }
-  ) {}; in
+  ),
+}:
+(
+  # Call flakeCompat with src pointing to the location of the flake.nix.
+  import flakeCompat { src = ./../../.; }
+).shellNix // (
+  # This expression is generating a shell that is merged with the one provided by shellNix.
+  let pkgs = import nixpkgs {}; in
   with pkgs;
   mkShell {
     CUSTOM_VAR_BOOL = if customVarBool then "true" else "false";
