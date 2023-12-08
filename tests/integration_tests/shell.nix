44eeb7366d11e4d3eaed5e5b12d5fea64e8a0eba
Chuck Grindel <chuck.grindel@gmail.com>
2023-12-08 08:17:41 -0700

    Refactored lock to be an arg

--- tests/integration_tests/shell.nix
+++ tests/integration_tests/shell.nix
@@ -1,10 +1,10 @@
 { 
   customVarBool ? false,
   customVarStr ? "default",
+  lock ? builtins.fromJSON (builtins.readFile ./../../flake.lock),
 }:
 (import
   (
-    let lock = builtins.fromJSON (builtins.readFile ./../../flake.lock); in
     fetchTarball {
       url = lock.nodes.flake-compat.locked.url or "https://github.com/edolstra/flake-compat/archive/${lock.nodes.flake-compat.locked.rev}.tar.gz";
       sha256 = lock.nodes.flake-compat.locked.narHash;
@@ -13,7 +13,6 @@
   { src = ./../../.; }
 ).shellNix // (
   let pkgs = import (
-    let lock = builtins.fromJSON (builtins.readFile ./../../flake.lock); in
     fetchTarball {
       url = "https://github.com/NixOS/nixpkgs/archive/${lock.nodes.nixpkgs.locked.rev}.tar.gz";
       sha256 = lock.nodes.nixpkgs.locked.narHash;
@@ -23,6 +22,5 @@
   mkShell {
     CUSTOM_VAR_BOOL = if customVarBool then "true" else "false";
     CUSTOM_VAR_STR = customVarStr;
-    # packages = [ nix ];
   }
 )
