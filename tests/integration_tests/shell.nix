8e5e7a6ec7fefeb8070a733d064fb3e9b79921e2
Chuck Grindel <chuck.grindel@gmail.com>
2023-12-08 10:19:55 -0700

    Debug code

--- tests/integration_tests/shell.nix
+++ tests/integration_tests/shell.nix
@@ -12,15 +12,16 @@
   )
   { src = ./../../.; }
 ).shellNix // (
-  let pkgs = import (
-    fetchTarball {
-      url = "https://github.com/NixOS/nixpkgs/archive/${lock.nodes.nixpkgs.locked.rev}.tar.gz";
-      sha256 = lock.nodes.nixpkgs.locked.narHash;
-    }
-  ) {}; in
-  with pkgs;
-  mkShell {
-    CUSTOM_VAR_BOOL = if customVarBool then "true" else "false";
-    CUSTOM_VAR_STR = customVarStr;
-  }
+  # let pkgs = import (
+  #   fetchTarball {
+  #     url = "https://github.com/NixOS/nixpkgs/archive/${lock.nodes.nixpkgs.locked.rev}.tar.gz";
+  #     sha256 = lock.nodes.nixpkgs.locked.narHash;
+  #   }
+  # ) {}; in
+  # with pkgs;
+  # mkShell {
+  #   CUSTOM_VAR_BOOL = if customVarBool then "true" else "false";
+  #   CUSTOM_VAR_STR = customVarStr;
+  # }
+  {}
 )
