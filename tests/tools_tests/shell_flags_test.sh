3e14d4b359cf27d18565087fefc5732635e76ae5
Chuck Grindel <chuck.grindel@gmail.com>
2023-12-15 14:11:56 -0700

    feat: support adding suggested shell flags to scripts

---
+++ tests/tools_tests/shell_flags_test.sh
@@ -0,0 +1,19 @@
+assert_msg="default flags will exit with an error code on failure"
+failed=false
+"${run_nix_shell_sh}" '
+[[ "true" == "false" ]]
+echo "This should not be seen."
+' > output.txt || failed=true
+output="$( <output.txt )"
+assert_equal "" "${output}" "${assert_msg}"
+assert_equal "true" "${failed}" "${assert_msg}"
+
+assert_msg="with no shell flags, script will exit without an error code on failure"
+success=false
+RNS_SHELL_FLAGS="false" "${run_nix_shell_sh}" '
+[[ "true" == "false" ]]
+echo "This should be seen."
+' > output.txt && success=true
+output="$( <output.txt )"
+assert_equal "This should be seen." "${output}" "${assert_msg}"
+assert_equal "true" "${success}" "${assert_msg}"
