378d101eab117a340450e5e91298a721aa9e87b3
Claudio Bley <claudio.bley@tweag.io>
2026-03-04 08:43:11 +0100

    flake: bazel_6 -> bazel_8
    Override the version specified in `.bazelversion` inside the dev shell, so it
    always matches the version from nixpkgs.

--- flake.nix
+++ flake.nix
@@ -17,17 +17,20 @@
       in {
         # ... and define a development shell for it ...
         devShells.default = with pkgs; mkShell {
-            # BEGIN Config to build Bazel 6
+          # BEGIN Config to build with Bazel 8
+          env = {
             # do not use Xcode on macOS
             BAZEL_USE_CPP_ONLY_TOOLCHAIN = "1";
             # for nixpkgs cc wrappers, select C++ explicitly (see https://github.com/NixOS/nixpkgs/issues/150655)
             BAZEL_CXXOPTS = "-x:c++";
-            buildInputs = lib.optional pkgs.stdenv.isDarwin darwin.cctools;
-            # END Config to build Bazel 6
-            # Name for the shell
-            name = "run_nix_shell_shell";
-            # ... which makes available the following dependencies, 
-            packages = [ bazel_6 bazel-buildtools cacert gcc nix git openssh ];
+            USE_BAZEL_VERSION = bazel_8.version;
           };
+          buildInputs = lib.optional pkgs.stdenv.isDarwin darwin.cctools;
+          # END Config to build Bazel 6
+          # Name for the shell
+          name = "run_nix_shell_shell";
+          # ... which makes available the following dependencies,
+          packages = [ bazel_8 bazel-buildtools cacert gcc nix git openssh ];
+        };
       });
 }
