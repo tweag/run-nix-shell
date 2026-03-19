8ae72eac585eb1eb2d9e1ecde82cf107f9e7ba27
Claudio Bley <claudio.bley@tweag.io>
2026-03-19 16:58:29 +0100

    Use ESM imports consistently

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 import * as core from "@actions/core";
 import * as exec from "@actions/exec";
-const path = require("path");
-const sh_join = require("shlex").join;
-const sh_split = require("shlex").split;
+import * as path from "node:path";
+import {join as sh_join, split as sh_split } from "shlex";
 
 async function run() {
   try {
